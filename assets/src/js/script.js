2b4abb5701548da7842cf66c9b3c0e06fe775466
Kawan Wagnner <kawanwagnner7@gmail.com>
2022-12-16 00:57:14 -0300

    Animações, mudanças no layout e atualização no menu.

--- assets/src/js/script.js
+++ assets/src/js/script.js
@@ -1,13 +1,12 @@
 const btn = document.querySelector("#menu-hamb")
 btn.addEventListener('click', () => {
     const navBar = document.querySelector('#navbar')
+    const active = document.querySelector('#active')
 
     navBar.classList.toggle('active')
     if (btn.innerHTML == 'X') {
-        btn.innerHTML = '<i class="fa-solid fa-bars"</i>';
-        document.body.style.overflow = 'auto'
+        btn.innerHTML = '<i class="fa-solid fa-bars"></i>';
     } else {
         btn.innerHTML = 'X';
-        document.body.style.overflow = 'hidden'
     }
 })
